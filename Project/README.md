126dc9c4ceed66259554cc9d548410eceac7eca3
Kieran McWilliams <Kieran@chmcsi.local>
2017-01-21 11:06:51 -0600

    New Folders

---
+++ Project/README.md
@@ -0,0 +1,9 @@
+# Relaxx
+Relaxx project for GETHIP 2016-17.
+
+## Team Members
+- Kristin Goertz (Project Manager)
+- Kieran McWilliams (Architect)
+- Rohan Badal
+- Adi Sethupathy
+- Ryan Melcher
---
+++ Project/lib/routes.js
@@ -0,0 +1,21 @@
+import {Videos} from '../imports/api/videos.js';
+
+Router.route('/music', function () {
+  this.render("music");
+});
+
+Router.route('/videos', function () {
+  this.render("videos");
+});
+
+Router.route('/games', function() {
+  this.render("games");
+});
+
+Router.route('/home', function() {
+  this.render("home");
+});
+
+Router.route('/', function() {
+  this.render("relaxx");
+});
